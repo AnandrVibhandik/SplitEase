e3ff2a2fb663a76bdac5e0d90949525c44a189b1
AnandrVibhandik <anandvibhandik23@gmail.com>
2026-08-18 01:05:12 +0530

    Some change

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 const AuthContext = createContext();
 
